1e1faaaf3be0f87ada92c2aeac06dc3028db571f
Atul Oli <161449224+27-Atul@users.noreply.github.com>
2024-07-20 17:05:49 +0530

    Add files via upload

---
+++ food-wala/src/utils/useRestraunt.js
@@ -0,0 +1,29 @@
+import { useState , useEffect} from "react";
+import RESTAURANT_MENU_API from "../contants";
+
+const useRestraunt = (id)=>{
+    useEffect(()=>{
+        getRestaurantInfo();
+    },[]);
+
+   
+
+    const [restaurant,setRestaurant]=useState(null);
+    // const[getRestaurantDetails,setGetRestaurantDetails]=useState(null)
+
+    async function getRestaurantInfo(){
+        
+        const data = await fetch("https://corsproxy.org/?"+encodeURIComponent("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5270362&lng=77.13593279999999&restaurantId="+ `${id}`));
+        const json = await data.json();
+        // console.log(json.data);
+        
+        
+        // feeDetails.restaurantId
+        setRestaurant(json.data);
+    }
+  
+    
+    return restaurant
+    
+}
+export default useRestraunt;
